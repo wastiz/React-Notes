c6556790e554cf7a696608780650930b32c5873d
Wastiz <valnos04@gmail.com>
2022-09-27 00:33:10 +0300

    another

--- marvel_starter/src/components/charInfo/CharInfo.js
+++ marvel_starter/src/components/charInfo/CharInfo.js
@@ -1,4 +1,5 @@
 import {Component} from 'react';
+import PropTypes from 'prop-types';
 import MarvelService from '../../services/MarvelService';
 import Spinner from '../spinner/spinner'
 import ErrorMessage from '../errorMessage/errorMessage';
